29277f30af1c9e189d0b20f8e20a8ec783d10924
akashg <akashg@webintensive.com>
2017-06-16 18:32:21 +0530

    Integrated lists to show.

--- nav_analyzer/funds_info/urls.py
+++ nav_analyzer/funds_info/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "funds_info"
 urlpatterns = [
-    url(r'^fund$', views.show_funds_list, name='index'),
+    url(r'^fund$', views.show_funds_list, name='show_funds_list'),
+    url(r'^fund/(?P<page_slug>[\w-]+)/', views.show_all_stocks, name='show_all_stocks'),    
 ]
--- nav_analyzer/funds_info/views.py
+++ nav_analyzer/funds_info/views.py
@@ -11,7 +11,12 @@ def show_funds_list(request):
     if distinct_funds:
         for fund in distinct_funds:
             fund = ''.join(fund)
-            funds_list.append(fund)
+            fund_object = Fund.objects.filter(fund_name=fund)[:1]
+            slug = fund_object[0].slug_url
+            data = {}
+            data["fund"] = fund
+            data["slug"] = slug
+            funds_list.append(data)
 
     funds_data["funds_list"] = funds_list
 
@@ -20,3 +25,19 @@ def show_funds_list(request):
     }
 
     return render(request, "funds_info/welcome.html", context)
+
+## View to show all stocks held by a fund
+def show_all_stocks(request, page_slug):    
+    print("page_slug: ", page_slug)
+    objects = Fund.objects.filter(slug_url=page_slug)
+    stocks_list = []
+    for obj in objects:
+        stock = obj.stock_name
+        stocks_list.append(stock)
+
+    print(stocks_list)
+
+    context = {
+        'stocks_list': stocks_list
+    }
+    return render(request, "funds_info/show_all_stocks.html", context)

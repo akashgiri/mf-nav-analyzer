45af96f0d60df5f20d9ce66d41085dc89752550b
akashg <akashg@webintensive.com>
2017-06-21 19:14:43 +0530

    Updated directory structure

--- nav_analyzer/core/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    #if request.user.is_authenticated():
-    return render(request, "core/welcome.html")
--- nav_analyzer/funds_info/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FundsInfoConfig(AppConfig):
-    name = 'funds_info'

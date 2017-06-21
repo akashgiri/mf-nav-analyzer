2bd58edf8977ffdde9cf9fc85829b67eda783d38
akashg <akashg@webintensive.com>
2017-06-21 19:08:54 +0530

    Updated settings

--- nav_analyzer/settings.py
+++ nav_analyzer/settings.py
@@ -152,6 +152,6 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 ALLOWED_SIGNUP_DOMAINS = ['*']
